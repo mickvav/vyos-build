5610986b28b10c4685dfc60253097a3fdd2e7ed3
Mihail Vasilev <mick@corp.linkintel.ru>
2016-04-25 11:03:15 +0000

    Cummulatives. Should be working mickvav fork.

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -4,12 +4,16 @@ for i in packages/*; do if [ -d $i ]; then git submodule update --init $i; fi; d
 
 if [ ! -f packages/linux-image-*-vyos_*.deb ]; then 
    echo "Rebuilding kernel"
-   (cp packages/vyos-kernel;LOCALVERSION= make-kpkg --rootcmd fakeroot --initrd --append_to_version -amd64-vyos --revision=4.4.6-1+vyos1+current1 kernel_source kernel_debug kernel_headers kernel_manual kernel_doc kernel_image)
+   (cd packages/vyos-kernel;LOCALVERSION= make-kpkg --rootcmd fakeroot --initrd --append_to_version -amd64-vyos --revision=4.4.6-1+vyos1+current1 kernel_source kernel_debug kernel_headers kernel_manual kernel_doc kernel_image)
 fi;
 
 for i in packages/*; do
-  if [ -d "$i" -a ! "$i" = "packages/vyos-kernel" -a ! -f ${i}*.deb ]; then
-    echo "Rebuilding $i"
-    (cd "$i"; dpkg-buildpackage -d -j3)
+  if [ -d "$i" ]; then
+    for package_name in `cat $i/debian/control| grep Package| awk '{print $2;};'`; do
+      if [ -d "$i" -a ! "$i" = "packages/vyos-kernel" -a ! -f "$package_name*.deb" ]; then
+        echo "Rebuilding $i"
+        (cd "$i"; dpkg-buildpackage -d -j3)
+      fi
+    done
   fi
 done
