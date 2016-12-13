9e84cd72e8b7e782eed7de6ff2272a80470b8dfd
Mihail Vasiliev <mick@corp.linkintel.ru>
2016-12-13 13:38:34 +0300

    Added vyatta-accel-ppp as submodule.

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get install build-essential git live-build pbuilder devscripts python-pystache autoconf automake autogen libncurses5-dev
+apt-get install build-essential git live-build pbuilder devscripts python-pystache autoconf automake autogen libncurses5-dev libssl-dev dh
--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NTHREADS=2
+
 for i in packages/*; do if [ -d $i ]; then git submodule update --init $i; fi; done
 
 if [ ! -f packages/linux-image-*-vyos_*.deb ]; then 
@@ -8,12 +10,20 @@ if [ ! -f packages/linux-image-*-vyos_*.deb ]; then
 fi;
 
 for i in packages/*; do
-  if [ -d "$i" ]; then
+  if [ "$i" = packages/xtables-addons ]; then
+     if [ -f packages/xtables-addons-common*.deb ]; then
+        echo "xtables-addons already built."
+     else
+        (cd "$i"; dpkg-vuildpackage -b -d -j$NTHREADS )
+     fi
+  else
+   if [ -d "$i" ]; then
     for package_name in `cat $i/debian/control| grep Package| awk '{print $2;};'`; do
       if [ -d "$i" -a ! "$i" = "packages/vyos-kernel" -a ! -f "$package_name*.deb" ]; then
         echo "Rebuilding $i"
-        (cd "$i"; dpkg-buildpackage -d -j3)
+        (cd "$i"; dpkg-buildpackage -d -j$NTHREADS )
       fi
     done
+   fi
   fi
 done
