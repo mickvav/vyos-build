3d9dc0100ea73e3e3d603680adfe02aa5e2a5bc8
Mihail Vasiliev <mick@corp.linkintel.ru>
2016-12-16 15:54:36 +0300

    Make this thing build again...

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -6,7 +6,7 @@ for i in packages/*; do if [ -d $i ]; then git submodule update --init $i; fi; d
 
 if [ ! -f packages/linux-image-*-vyos_*.deb ]; then 
    echo "Rebuilding kernel"
-   (cd packages/vyos-kernel;LOCALVERSION= make-kpkg --rootcmd fakeroot --initrd --append_to_version -amd64-vyos --revision=4.4.6-1+vyos1+current1 kernel_source kernel_debug kernel_headers kernel_manual kernel_doc kernel_image)
+   (cd packages/vyos-kernel;LOCALVERSION= make-kpkg --rootcmd fakeroot --initrd --append_to_version -amd64-vyos --revision=mickvav kernel_source kernel_debug kernel_headers kernel_manual kernel_doc kernel_image)
 fi;
 
 for i in packages/*; do
